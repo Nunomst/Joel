e5428b34879dc49407adc387fc42e13e8e808130
Paulo <paulo.barros.t0123941@edu.atec.pt>
2023-06-15 23:42:36 +0100

    Navbar desktop + mobile ainda por componentar

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
+let lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+       navbar.classList.add('icon');
+    } else {
+      navbar.classList.remove('icon');
+    }
+    lastScrollTop = scrollTop;
+})
 
-            fetch('https://fakestoreapi.com/products',{
-              method:"POST",
-              body:JSON.stringify(
-                  {
-                      category: 'electronic',
-                      description: 'lorem ipsum set',
-                      image: 'https://i.pravatar.cc',
-                      price: 13.5,
-                      rating: {
-                          rate: 4.5,
-                          count: 12
-                      },
-                      title: 'test product'
-                      
-                  }
-              )
-          })
-              .then(res=>res.json())
-              .then(json=>console.log(json))
-  
 
-let cartIcon = document.getElementById("cart-icon");
-
-cartIcon.addEventListener("mouseover", function(){
-    cartIcon.style.color = "#DEBC42"
-    cartIcon.classList.add("fa-beat");
-});
-
-cartIcon.addEventListener("mouseout", function() {
-  cartIcon.classList.remove("fa-beat");
-  cartIcon.style.color = "black";
-});
+function change(item){
+  const buttons = document.querySelectorAll('ion-icon');
+  buttons.forEach(function(obj){
+      obj.classList.remove("active");
+  });
+  item.classList.add("active");
+}
