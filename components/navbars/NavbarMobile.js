15d3799d874d572fcf08fd6a7d6fd5dc3aa1722c
Paulo <paulo.barros.t0123941@edu.atec.pt>
2023-06-23 18:55:15 +0100

    COM NAVBAR FINAL DESKTOP E MOBILE

--- components/navbars/NavbarMobile.js
+++ components/navbars/NavbarMobile.js
@@ -2,12 +2,13 @@ import { highlightNavItem } from "../../logic/navbar/highlightNavItem";
 
 export function navbarMobile() {
     let navbar = document.createElement('div');
-    navbar.innerHTML = `<section>
+    navbar.innerHTML = `  <section>
+    
     <div class="bottom-bar">
-      <ion-icon name="pricetags-outline" class="icon"></ion-icon>
-      <ion-icon name="cart-outline" class="icon"></ion-icon>
-      <ion-icon name="reader-outline" class="icon"></ion-icon>
-      <ion-icon name="mail-outline" class="icon"></ion-icon>
+      <ion-icon name="pricetags-outline" class="icon" onclick="change(this)"></ion-icon>
+      <ion-icon name="reader-outline" class="icon" onclick="change(this)"></ion-icon>
+      <ion-icon name="mail-outline" class="icon" onclick="change(this)"></ion-icon>
+        <ion-icon name="cart-outline" class="icon" onclick="change(this)"></ion-icon>
     </div>
   </section>`;
 
