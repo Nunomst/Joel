7c48646baf3e849ed3b3ad8a43c4ef3d664b27e8
Paulo <paulo.barros.t0123941@edu.atec.pt>
2023-07-07 23:17:54 +0100

    fiz estupidez ao componentar

--- main.js
+++ main.js
@@ -3,18 +3,13 @@ import {navbarDesktop} from './components/navbars/NavbarDesktop.js';
 import {navbarMobile} from './components/navbars/NavbarMobile.js';
 import { handleScroll } from './logic/navbar/scrollLogic';
 import {highlightNavItem} from './logic/navbar/highlightNavItem';
-import { cartTable } from './components/cart/cartTable';
 import {footer} from './components/footer/footer.js';
-import { checkout } from './components/checkout/checkout';
-
 
 
 let appDiv = document.getElementById('app');
 
 appDiv.appendChild(navbarDesktop());
 appDiv.appendChild(navbarMobile());
-appDiv.appendChild(cartTable());
-appDiv.appendChild(checkout());
 appDiv.appendChild(footer());
 
 
